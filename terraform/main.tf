a54833d7cb78ec1ff5818fd51475fd799497e99b
Adam Maas <adamjm1220@gmail.com>
2024-11-07 15:03:48 -0500

    VErification_IAC

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+data "aws_ecr_image" "latest_image" {
+  repository_name = "youtube-containers"
+  image_tag       = "youtube-job-verification"
+}
+
+data "aws_iam_role" "existing_lambda_role" {
+  name = "lambda_role_for_s3_access" #Name of the iam role that gives your lambda permissions
+}
+
+resource "aws_lambda_function" "api_lambda" {
+  function_name = "youtube-job-verification"
+  role          = data.aws_iam_role.existing_lambda_role.arn
+
+  package_type = "Image"
+  image_uri    = "${data.aws_ecr_image.latest_image.image_uri}"
+
+  environment {
+    variables = {
+      Api_Key = var.Api_Key 
+    }
+  }
+
+  timeout = 30 #Adjust the timeout of the function IN SECONDS
+  memory_size = 512 #Adjust the Memory of the function
+}
+
+resource "aws_lambda_function_url" "lambda_url" {
+  function_name = aws_lambda_function.api_lambda.function_name #Gives the AWS Lambda function a URL for us to trigger
+  authorization_type = "NONE"
+}
+
